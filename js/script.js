600de9f502b9ff2cdbe8f2648187aa9ebb6f2f7d
douglas <douglas.arau@hotmail.com>
2025-04-24 14:36:41 -0300

    fix: arrumando legibilidade do codigo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//asdasd
 const container = document.getElementById('jogo');
 const colunas = 13;
 const linhas = 13;
@@ -7,30 +8,30 @@ let pontos = 0;
 let vidas = 3;
 
 function criarGrid() {
-  for (let i = 0; i < totalCelulas; i++) {
+  for (let index = 0; index < totalCelulas; index++) {
     const celula = document.createElement('div');
     celula.classList.add('celula');
     container.appendChild(celula);
   }
 
   const celulas = container.querySelectorAll('.celula');
-  for (let i = 0; i <= colunas; i += 3) {
-    celulas[i].classList.add('toca');
+  for (let index = 0; index <= colunas; index += 3) {
+    celulas[index].classList.add('toca');
   }
 
   for (let linha = 6; linha <= 6; linha++) {
     const inicio = linha * colunas;
     const fim = inicio + colunas;
-    for (let i = inicio; i < fim; i++) {
-      celulas[i].classList.add('chao');
+    for (let index = inicio; index < fim; index++) {
+      celulas[index].classList.add('chao');
     }
   }
 
   for (let linha = 12; linha <= 12; linha++) {
     const inicio = linha * colunas;
     const fim = inicio + colunas;
-    for (let i = inicio; i < fim; i++) {
-      celulas[i].classList.add('chao');
+    for (let index = inicio; index < fim; index++) {
+      celulas[index].classList.add('chao');
     }
   }
   console.log(celulas)
